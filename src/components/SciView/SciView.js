6751447f75058c80eb10e19bc52c6081aff59d30
unknown <ma_dong_liang@163.com>
2023-03-17 16:57:11 +0800

    finish SciView component

--- src/components/SciView/SciView.js
+++ src/components/SciView/SciView.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {setViewPosition} from './SciViewLogic.js';
+import {setViewTitle, setViewPosition, setProportion} from './SciViewLogic.js';
 
 function SciView(props) {
   return (
     <section style={setViewPosition(props)}>
-      {props.children}
+      {setViewTitle(props)}
+      <section style={setProportion()}>
+        {props.children}
+      </section>
     </section>
   );
 }
@@ -14,7 +17,21 @@ function SciView(props) {
 // 属性类型和必要性限制
 SciView.propTypes = {
   // 视图位置，'起始行/终止行/起始列/终止列'
-  position: PropTypes.string.isRequired
+  position: PropTypes.string.isRequired,
+  // 是否需要Title
+  title: PropTypes.string,
+  // title是否需要占用视图位置，true占用 | false不占用
+  titleOccupy: PropTypes.bool,
+  // title文本位置，值有left | center | right，默认center
+  titlePosition: PropTypes.string,
+  // title背景颜色，默认transparent
+  titleBgColor: PropTypes.string,
+  // title文本颜色，默认#000
+  titleColor: PropTypes.string,
+  // title文本是否加粗，默认normal
+  titleWeight: PropTypes.number,
+  // title字体大小，默认1rem
+  titleFontSize: PropTypes.string
 };
 
 export default SciView;
--- src/components/SciView/SciViewLogic.js
+++ src/components/SciView/SciViewLogic.js
@@ -1,8 +1,38 @@
+import React from 'react';
+
+// 设置title
+export const setViewTitle = (props)=>{
+    const {title, titleOccupy, titlePosition, titleBgColor, titleColor, titleWeight, titleFontSize} = props;
+    return (
+        title ? <p style={{
+            margin: 0,
+            width: '100%',
+            position: titleOccupy ? 'static' : 'absolute',
+            textAlign: titlePosition || 'center',
+            backgroundColor: titleBgColor || 'transparent',
+            color: titleColor || '#000',
+            fontWeight: titleWeight || 'normal',
+            fontSize: titleFontSize || '1rem'
+        }}>{title}</p> : null
+    );
+}
+
+// 确定视图绘制区域位置
 export const setViewPosition = (props)=>{
     let {position} = props;
     position = position.split('/');
     return {
         gridColumn: `${Number(position[2])} / ${Number(position[3]) + 1}`,
         gridRow: `${Number(position[0])} / ${Number(position[1]) + 1}`,
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'relative'
+    }
+}
+
+// 设置title和视图容器大小占比
+export const setProportion = ()=>{
+    return {
+        flexGrow: '1'
     }
 }
